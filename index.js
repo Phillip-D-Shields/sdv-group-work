cd5b9d19d92da31550c419adb855ca7ccb5d4362
Knightron1 <61396001+Knightron1@users.noreply.github.com>
2020-03-03 10:12:18 +1300

    more math

---
+++ index.js
@@ -0,0 +1,14 @@
+var a = 2, b = 3, c = 4, d = 5, e = 6;
+
+var sum = a + b;
+console.log(sum);
+
+var sub = b - a;
+console.log(sub);
+var div = c / a;
+console.log(div);
+var perc = a % c;
+console.log(perc);
+
+var mult = a * b;
+console.log("the multiple is " + mult);
