70689a8d04a242265833970629ea7502828ea841
Knightron1 <61396001+Knightron1@users.noreply.github.com>
2020-03-03 10:22:17 +1300

    added comments

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var a = 2, b = 3, c = 4, d = 5, e = 6;
 
 var sum = a + b;
-console.log(sum);
-
+console.log("a + b = " + sum);
+// This equation shows addition
 var sub = b - a;
-console.log(sub);
+console.log("b - a = " + sub);
+// This equation shows subtraction
 var div = c / a;
-console.log(div);
+console.log("c / a =" + div);
+// This equation shows division
 var perc = a % c;
-console.log(perc);
+console.log("a % c = " + perc);
+// This equation shows percentage
 
 var mult = a * b;
-console.log("the multiple is " + mult);
+console.log("a * b = " + mult);
+// This equation shows multiplication
